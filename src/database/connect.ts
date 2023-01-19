88d363133a0db84a2465eedb29c0fc6431f1a5c5
YhyaIbrahim <113423953+YhyaGithub@users.noreply.github.com>
2023-01-19 18:02:04 +0000

    Bug Fixes

--- src/database/connect.ts
+++ src/database/connect.ts
@@ -1,10 +1,12 @@
 import { connect } from 'mongoose';
 import process from 'node:process';
+import logger from '../utilities/logger';
 
 async function Connect() {
     const uri = process.env.MONGODBURI;
     if(!uri) return;
     const conn = await connect(uri);
+    if(conn) logger.info(`Successfully Connected To Database`);
     return conn;
 }
 
--- src/utilities/requests.ts
+++ src/utilities/requests.ts
@@ -1,18 +1,22 @@
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
 import { TOKEN } from '../config';
-import { APIGuild } from 'discord-api-types/v10';
+import { APIGuild, APIGuildMember } from 'discord-api-types/v10';
 
 export const getGuildById = async (id: string) => {
     let guild = await axios({
             "url": `https://discord.com/api/v10/guilds/${id}`,
             "method": "get",
             "headers": {"authorization": `Bot ${TOKEN}`}
-    })
+    }).catch(() => {
+        return undefined 
+    });
+    if(!guild) return undefined;
+    
     let members = await axios({
-            "url": `https://discord.com/api/v10/guilds/${id}/members`,
+            "url": `https://discord.com/api/v10/guilds/${guild.data.id}/members`,
             "method": "get",
             "headers": {"authorization": `Bot ${TOKEN}`}
     })
-    guild.data.members = members.data
-        return guild.data;
+    guild.data.members = members.data;
+    return guild.data;
 }
